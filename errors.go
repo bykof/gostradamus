02c91b640656325babe01eea4560d7410939792b
Michael Bykovski <michael.bykovskii@aoe.com>
2020-04-07 19:43:25 +0200

    Fix linting

--- errors.go
+++ errors.go
@@ -1,10 +1,9 @@
 package gostradamus
 
 import (
-	"errors"
 	"fmt"
 )
 
 func FormatTokenIsNotMapped(formatToken string) error {
-	return errors.New(fmt.Sprintf("FormatToken: %s is not mapped", formatToken))
+	return fmt.Errorf("FormatToken: %s is not mapped", formatToken)
 }
