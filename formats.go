1ce6aef4939f2600106f0bb82f0be10d41c72fa1
Michael Bykovski <michael.bykovskii@aoe.com>
2020-04-07 20:46:54 +0200

    fix format

--- formats.go
+++ formats.go
@@ -3,7 +3,7 @@ package gostradamus
 const (
 	// Iso8601 format
 	// Example: 2012-12-12T12:12:12.000000
-	Iso8601   = "YYYY-MM-DDTHH:mm:ss.S"
+	Iso8601 = "YYYY-MM-DDTHH:mm:ss.S"
 
 	// Iso8601TZ format with timezone
 	// Example with UTC: 2012-12-12T12:12:12.000000Z
@@ -12,5 +12,5 @@ const (
 
 	// CTime format with ctime standard
 	// Example: Sat Jan 19 18:26:50 2019
-	CTime     = "ddd MMM DD HH:mm:ss YYYY"
+	CTime = "ddd MMM DD HH:mm:ss YYYY"
 )
